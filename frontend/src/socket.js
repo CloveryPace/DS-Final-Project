7ebe52865663266c27e6249821fd0d943f8a06f0
CloveryPace <128057141+CloveryPace@users.noreply.github.com>
2024-12-23 23:18:12 +0800

    Remove unwanted files

--- frontend/src/socket.js
+++
@@ -1,8 +0,0 @@
-import { io } from 'socket.io-client';
-
-// "undefined" means the URL will be computed from the `window.location` object
-const URL = 'localhost:3000/websocket';
-
-export const socket = io(URL, {
-	autoConnect: false,
-});
